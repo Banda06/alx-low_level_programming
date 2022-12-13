6a5e70095f9ce63a2f52b39c8f95b0b253b28a3f
Banda06 <williamntsikelelo@gmail.com>
2022-12-12 20:09:55 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
-#!/bin/bash
-#include <stdio.h>
-int main(void)
-{
+#include <stdio.>
+int main() {
+	//displays the string and a new line
 	printf("with proper grammar,but the outcome is a piece of art,\n");
-	return(0);
+	return 0;
 }
