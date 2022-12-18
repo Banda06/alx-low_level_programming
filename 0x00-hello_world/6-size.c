319bfe89793a79c6686bbe9c452ccf75b519604f
Banda06 <williamntsikelelo@gmail.com>
2022-12-18 04:38:31 -0800

    sice of c types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	int b;
+	long int c;
+	long long int d;
+	char a;
+	float e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size if an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size if an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
