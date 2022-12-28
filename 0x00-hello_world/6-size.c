7b503b006325dbeb640cd60b590d14f4764abcdc
Banda06 <williamntsikelelo@gmail.com>
2022-12-28 08:06:49 -0800

    Prints the size of types of a computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,20 @@
 
 /**
  * main - Entry point
- * Return:0 always
+ * Return: 0 always
  */
 int main(void)
 {
-	int b;
-	long int c;
-	long long int d;
-	char a;
-	float e;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size if an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size if an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
