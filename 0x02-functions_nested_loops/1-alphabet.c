a8d9a4b0bac991d8b1a80df80553fbdda0702862
Banda06 <williamntsikelelo@gmail.com>
2023-10-10 14:27:34 -0700

    prints prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * description: prints_alphabet -main entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
