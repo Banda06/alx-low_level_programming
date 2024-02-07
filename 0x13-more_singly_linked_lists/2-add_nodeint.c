fab5e74ead26e81a3b597ad274212379a52a76a4
Banda06 <williamntsikelelo@gmail.com>
2024-02-07 13:58:25 -0800

    A function that adds a new node at the beginning of a listint_t list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,32 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * add_nodeint -  function that adds a new node at the beginning of,
+ * a listint_t list.
+ * @head: pointer to the pointer of head of linked list.
+ * @n: data to add to the list.
+ *
+ * Return: the address of the new element, or NULL if it failed.
  */
-int main(void)
-{
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	/**
+	 * Algorithm
+	 * 1. Create a new node with the given data.
+	 * 2. Make the new node point to the head node.
+	 * 3. Finally, make the new node as the head node.
+	 */
+
+	listint_t *new_node; /* listint_t is a data type */
+	/* Create a new node */
+	new_node = malloc(sizeof(listint_t));
+	/* check if malloc has failed and return null */
+	if (new_node == NULL)
+		return (NULL);
+	/* access the n field of the struct and initialize it with n */
+	/* which has been passed to the function */
+	new_node->n = n;
+	/* since we are adding to the beginnin of the list then the */
+	/* next field should point to the beginning of the current list */
+	new_node->next = *head;
+	/* make the new_node as the head node */
+	*head = new_node;
+	/* return the address of the new element */
+	return (new_node);
 }
