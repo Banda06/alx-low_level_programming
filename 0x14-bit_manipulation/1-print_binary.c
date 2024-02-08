5d7078b2d90a44c923ac7a1712e3b7dbd8dbfb6e
Banda06 <williamntsikelelo@gmail.com>
2024-02-08 12:41:07 -0800

    a function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	/* Base case: if the number is greater than 1, continue recursion*/
+	if (n > 1)
+		print_binary(n >> 1);/* Right shift to get the next bit */
+
+	/* Print the least significant bit (LSB) of the number*/
+	_putchar((n & 1) + '0');/* Use bitwise AND to get the LSB and convert to ASCII*/
+
+/* The recursion and printing build up the binary representation from the most significant bit (MSB)*/
+
+}
