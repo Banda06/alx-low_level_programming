1405caa2073684f938a9eff0a383f19ce872c826
Banda06 <williamntsikelelo@gmail.com>
2024-02-16 14:10:18 -0800

    creates a functiont that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	/*Open the file with create, write-only,
-	 * and truncate options, with permissions 0600*/
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -36,8 +35,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (rwr == -1)
 		return (-1);
-	/* close file*/
+
 	close(fd);
-	/*returns 1 if successful*/
+
 	return (1);
 }
