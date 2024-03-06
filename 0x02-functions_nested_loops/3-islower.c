c85f2b97b7f52c30167afdde6f2b3e7ee388b8b0
Banda06 <williamntsikelelo@gmail.com>
2024-03-05 21:27:41 -0800

    checks and prints lower case characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,7 @@
 
 /**
  * _islower - chech if the given character is lower case.
+ *
  * @c: character to test.
  *
  * Return: 1 if (true), else 0 (false)
@@ -11,9 +12,10 @@
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-
+	{
 		return (1);
 
+	}
 	else
 
 		return (0);
